f07c1326d311e94bd8fa06084d999e23cb4be730
Rossi21 <rossi21@gmail.com>
2023-07-25 12:54:02 -0700

    index form

--- shows_app/urls.py
+++ shows_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('shows/new', views.index),
+    path('shows/create',views.create_show),
 ]   
--- shows_app/views.py
+++ shows_app/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from shows_app.models import Show
 
 def index(request):
     return render(request, "index.html")
+
+def create_show(request):
+        
+        title = request.POST['title']
+        network = request.POST['network']
+        release_date = request.POST['release_date']
+        description = request.POST['description']
+        
+
+        show = Show.objects.create(title = title, network = network, release_date = release_date, description = description)
+        
+        return redirect("/shows/new")
+
 
